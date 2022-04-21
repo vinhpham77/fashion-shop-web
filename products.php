c37d23a14c267f6f95dacca8d57a38ff2d7cd2af
Vin <phamvanvinh8a2@gmail.com>
2022-04-22 02:21:50 +0700

    Sản phẩm

---
+++ products.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Trang sản phẩm</title>
+	<link rel="stylesheet" href="style/products.css">
+	<link rel="stylesheet" href="style/reset.css">
+	<link rel="stylesheet" href="fontawesome/css/all.css">
+</head>
+<body>
+	<div class="products__block">
+		<div class="product">
+			<div class="product__avatar">
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ac8c6ac4-f054-4bec-b48b-51c704ef85c0/df3ghib-40ba3771-0ac8-491d-839e-ae294b07a009.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FjOGM2YWM0LWYwNTQtNGJlYy1iNDhiLTUxYzcwNGVmODVjMFwvZGYzZ2hpYi00MGJhMzc3MS0wYWM4LTQ5MWQtODM5ZS1hZTI5NGIwN2EwMDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J0SnuJ3MwcCY0OlIBD_8iPTzhTUgXvSDOvzTsnmvDyU" alt="Không ảnh">
+			</div>
+			<div class="product__name">Sản phẩm XYZ</div>
+			<div class="product__sale">
+				<span class="product-sale__price">300.000đ</span>
+				<a class="product-sale__btn"><i class="fa-solid fa-cart-plus"></i></a>
+			</div>
+		</div>
+		<div class="product">
+			<div class="product__avatar">
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ac8c6ac4-f054-4bec-b48b-51c704ef85c0/df3ghib-40ba3771-0ac8-491d-839e-ae294b07a009.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FjOGM2YWM0LWYwNTQtNGJlYy1iNDhiLTUxYzcwNGVmODVjMFwvZGYzZ2hpYi00MGJhMzc3MS0wYWM4LTQ5MWQtODM5ZS1hZTI5NGIwN2EwMDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J0SnuJ3MwcCY0OlIBD_8iPTzhTUgXvSDOvzTsnmvDyU" alt="Không ảnh">
+			</div>
+			<div class="product__name">Sản phẩm XYZ</div>
+			<div class="product__sale">
+				<span class="product-sale__price">300.000đ</span>
+				<a class="product-sale__btn"><i class="fa-solid fa-cart-plus"></i></a>
+			</div>
+		</div>
+		<div class="product">
+			<div class="product__avatar">
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ac8c6ac4-f054-4bec-b48b-51c704ef85c0/df3ghib-40ba3771-0ac8-491d-839e-ae294b07a009.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FjOGM2YWM0LWYwNTQtNGJlYy1iNDhiLTUxYzcwNGVmODVjMFwvZGYzZ2hpYi00MGJhMzc3MS0wYWM4LTQ5MWQtODM5ZS1hZTI5NGIwN2EwMDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J0SnuJ3MwcCY0OlIBD_8iPTzhTUgXvSDOvzTsnmvDyU" alt="Không ảnh">
+			</div>
+			<div class="product__name">Sản phẩm XYZ</div>
+			<div class="product__sale">
+				<span class="product-sale__price">300.000đ</span>
+				<a class="product-sale__btn"><i class="fa-solid fa-cart-plus"></i></a>
+			</div>
+		</div>
+		<div class="product">
+			<div class="product__avatar">
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ac8c6ac4-f054-4bec-b48b-51c704ef85c0/df3ghib-40ba3771-0ac8-491d-839e-ae294b07a009.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FjOGM2YWM0LWYwNTQtNGJlYy1iNDhiLTUxYzcwNGVmODVjMFwvZGYzZ2hpYi00MGJhMzc3MS0wYWM4LTQ5MWQtODM5ZS1hZTI5NGIwN2EwMDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J0SnuJ3MwcCY0OlIBD_8iPTzhTUgXvSDOvzTsnmvDyU" alt="Không ảnh">
+			</div>
+			<div class="product__name">Sản phẩm XYZ</div>
+			<div class="product__sale">
+				<span class="product-sale__price">300.000đ</span>
+				<a class="product-sale__btn"><i class="fa-solid fa-cart-plus"></i></a>
+			</div>
+		</div>
+		<div class="product">
+			<div class="product__avatar">
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ac8c6ac4-f054-4bec-b48b-51c704ef85c0/df3ghib-40ba3771-0ac8-491d-839e-ae294b07a009.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FjOGM2YWM0LWYwNTQtNGJlYy1iNDhiLTUxYzcwNGVmODVjMFwvZGYzZ2hpYi00MGJhMzc3MS0wYWM4LTQ5MWQtODM5ZS1hZTI5NGIwN2EwMDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J0SnuJ3MwcCY0OlIBD_8iPTzhTUgXvSDOvzTsnmvDyU" alt="Không ảnh">
+			</div>
+			<div class="product__name">Sản phẩm XYZ</div>
+			<div class="product__sale">
+				<span class="product-sale__price">300.000đ</span>
+				<a class="product-sale__btn"><i class="fa-solid fa-cart-plus"></i></a>
+			</div>
+		</div>
+		<div class="product">
+			<div class="product__avatar">
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ac8c6ac4-f054-4bec-b48b-51c704ef85c0/df3ghib-40ba3771-0ac8-491d-839e-ae294b07a009.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FjOGM2YWM0LWYwNTQtNGJlYy1iNDhiLTUxYzcwNGVmODVjMFwvZGYzZ2hpYi00MGJhMzc3MS0wYWM4LTQ5MWQtODM5ZS1hZTI5NGIwN2EwMDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J0SnuJ3MwcCY0OlIBD_8iPTzhTUgXvSDOvzTsnmvDyU" alt="Không ảnh">
+			</div>
+			<div class="product__name">Sản phẩm XYZ</div>
+			<div class="product__sale">
+				<span class="product-sale__price">300.000đ</span>
+				<a class="product-sale__btn"><i class="fa-solid fa-cart-plus"></i></a>
+			</div>
+		</div>
+		<div class="product">
+			<div class="product__avatar">
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ac8c6ac4-f054-4bec-b48b-51c704ef85c0/df3ghib-40ba3771-0ac8-491d-839e-ae294b07a009.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FjOGM2YWM0LWYwNTQtNGJlYy1iNDhiLTUxYzcwNGVmODVjMFwvZGYzZ2hpYi00MGJhMzc3MS0wYWM4LTQ5MWQtODM5ZS1hZTI5NGIwN2EwMDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J0SnuJ3MwcCY0OlIBD_8iPTzhTUgXvSDOvzTsnmvDyU" alt="Không ảnh">
+			</div>
+			<div class="product__name">Sản phẩm XYZ</div>
+			<div class="product__sale">
+				<span class="product-sale__price">300.000đ</span>
+				<a class="product-sale__btn"><i class="fa-solid fa-cart-plus"></i></a>
+			</div>
+		</div>
+		<div class="product">
+			<div class="product__avatar">
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ac8c6ac4-f054-4bec-b48b-51c704ef85c0/df3ghib-40ba3771-0ac8-491d-839e-ae294b07a009.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FjOGM2YWM0LWYwNTQtNGJlYy1iNDhiLTUxYzcwNGVmODVjMFwvZGYzZ2hpYi00MGJhMzc3MS0wYWM4LTQ5MWQtODM5ZS1hZTI5NGIwN2EwMDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J0SnuJ3MwcCY0OlIBD_8iPTzhTUgXvSDOvzTsnmvDyU" alt="Không ảnh">
+			</div>
+			<div class="product__name">Sản phẩm XYZ</div>
+			<div class="product__sale">
+				<span class="product-sale__price">300.000đ</span>
+				<a class="product-sale__btn"><i class="fa-solid fa-cart-plus"></i></a>
+			</div>
+		</div>
+		<div class="product">
+			<div class="product__avatar">
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ac8c6ac4-f054-4bec-b48b-51c704ef85c0/df3ghib-40ba3771-0ac8-491d-839e-ae294b07a009.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FjOGM2YWM0LWYwNTQtNGJlYy1iNDhiLTUxYzcwNGVmODVjMFwvZGYzZ2hpYi00MGJhMzc3MS0wYWM4LTQ5MWQtODM5ZS1hZTI5NGIwN2EwMDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J0SnuJ3MwcCY0OlIBD_8iPTzhTUgXvSDOvzTsnmvDyU" alt="Không ảnh">
+			</div>
+			<div class="product__name">Sản phẩm XYZ</div>
+			<div class="product__sale">
+				<span class="product-sale__price">300.000đ</span>
+				<a class="product-sale__btn"><i class="fa-solid fa-cart-plus"></i></a>
+			</div>
+		</div>
+		<div class="product">
+			<div class="product__avatar">
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ac8c6ac4-f054-4bec-b48b-51c704ef85c0/df3ghib-40ba3771-0ac8-491d-839e-ae294b07a009.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FjOGM2YWM0LWYwNTQtNGJlYy1iNDhiLTUxYzcwNGVmODVjMFwvZGYzZ2hpYi00MGJhMzc3MS0wYWM4LTQ5MWQtODM5ZS1hZTI5NGIwN2EwMDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J0SnuJ3MwcCY0OlIBD_8iPTzhTUgXvSDOvzTsnmvDyU" alt="Không ảnh">
+			</div>
+			<div class="product__name">Sản phẩm XYZ</div>
+			<div class="product__sale">
+				<span class="product-sale__price">300.000đ</span>
+				<a class="product-sale__btn"><i class="fa-solid fa-cart-plus"></i></a>
+			</div>
+		</div>
+		<div class="product">
+			<div class="product__avatar">
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ac8c6ac4-f054-4bec-b48b-51c704ef85c0/df3ghib-40ba3771-0ac8-491d-839e-ae294b07a009.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FjOGM2YWM0LWYwNTQtNGJlYy1iNDhiLTUxYzcwNGVmODVjMFwvZGYzZ2hpYi00MGJhMzc3MS0wYWM4LTQ5MWQtODM5ZS1hZTI5NGIwN2EwMDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J0SnuJ3MwcCY0OlIBD_8iPTzhTUgXvSDOvzTsnmvDyU" alt="Không ảnh">
+			</div>
+			<div class="product__name">Sản phẩm XYZ</div>
+			<div class="product__sale">
+				<span class="product-sale__price">300.000đ</span>
+				<a class="product-sale__btn"><i class="fa-solid fa-cart-plus"></i></a>
+			</div>
+		</div>
+		<div class="product">
+			<div class="product__avatar">
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ac8c6ac4-f054-4bec-b48b-51c704ef85c0/df3ghib-40ba3771-0ac8-491d-839e-ae294b07a009.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FjOGM2YWM0LWYwNTQtNGJlYy1iNDhiLTUxYzcwNGVmODVjMFwvZGYzZ2hpYi00MGJhMzc3MS0wYWM4LTQ5MWQtODM5ZS1hZTI5NGIwN2EwMDkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J0SnuJ3MwcCY0OlIBD_8iPTzhTUgXvSDOvzTsnmvDyU" alt="Không ảnh">
+			</div>
+			<div class="product__name">Sản phẩm XYZ</div>
+			<div class="product__sale">
+				<span class="product-sale__price">300.000đ</span>
+				<a class="product-sale__btn"><i class="fa-solid fa-cart-plus"></i></a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
