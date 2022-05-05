0d8d8b4f782bc3209521f682b8da2b5edcd166bc
nguyenhongban0210 <104150893+nguyenhongban0210@users.noreply.github.com>
2022-05-05 20:09:17 +0700

    DanhMucSP

---
+++ js/category.js
@@ -0,0 +1,6 @@
+const itemsliderbar = document.querySelectorAll(".cartegory-left-li")
+itemsliderbar.forEach(function(menu,index){
+    menu.addEventListener("click",function(){
+        menu.classList.toggle("block")
+    })
+})
