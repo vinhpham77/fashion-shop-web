4475b933d1f37cc30e39667763f4fcfba93b3348
Vin <phamvanvinh8a2@gmail.com>
2022-05-08 18:37:14 +0700

    Updates product-detail

--- js/product-detail.js
+++ js/product-detail.js
@@ -2,7 +2,6 @@ let bigImg = document.querySelector('.gallery--big img');
 let zoom = document.querySelector('.gallery--zoom');
 let innerImg = document.querySelector('.gallery__inner img');
 let images = document.querySelectorAll('.gallery--small img');
-let sizes = document.querySelectorAll('.product-detail__size span');
 let exit = document.querySelector('.controller__exit');
 let next = document.querySelector('.controller__next');
 let prev = document.querySelector('.controller__prev');
@@ -18,16 +17,6 @@ images.forEach((item, index) => {
     })
 })
 
-sizes.forEach((item, index) => {
-    item.addEventListener('click', function() {
-        let checkedSize = document.querySelector('.size--checked');
-        if (checkedSize !== null) {
-            checkedSize.classList = '';
-        }
-        sizes[index].classList = 'size--checked checked';
-    })
-})
-
 bigImg.addEventListener('click', function() {
     zoom.classList.add('show');
     showBigImg();
@@ -39,17 +28,17 @@ exit.addEventListener('click', function() {
 
 function showBigImg() {
     innerImg.src = images[currentIndex].src;
-    
+
     if (currentIndex === 0) {
-    	prev.classList.add('hide');
+        prev.classList.add('hide');
     } else {
-    	prev.classList.remove('hide');
+        prev.classList.remove('hide');
     }
 
     if (currentIndex === images.length - 1) {
-    	next.classList.add('hide');
+        next.classList.add('hide');
     } else {
-    	next.classList.remove('hide');
+        next.classList.remove('hide');
     }
 }
 
@@ -64,5 +53,95 @@ prev.addEventListener('click', function() {
     if (currentIndex > 0) {
         currentIndex--;
         showBigImg();
+    }
+})
+
+let sizes = document.querySelectorAll('.product-detail__size > span');
+let sizeName = document.querySelector('.product-detail__size > p > span');
+
+sizes.forEach((item, index) => {
+    item.addEventListener('click', function() {
+        let checkedSize = document.querySelector('.size--checked');
+        if (checkedSize !== null) {
+            checkedSize.classList = '';
+        }
+        sizes[index].classList = 'size--checked checked';
+        sizeName.innerHTML = sizes[index].innerHTML;
+    })
+})
+
+let plus = document.querySelector('.fa-plus');
+let minus = document.querySelector('.fa-minus');
+let quantityInput = document.querySelector('#quantity');
+let quantityOnHand = 999;
+
+plus.addEventListener('click', function() {
+    if (quantityInput.value < quantityOnHand) {
+        quantityInput.value++;
+    }
+})
+
+minus.addEventListener('click', function() {
+    if (quantityInput.value > 1) {
+        quantityInput.value--;
+    }
+})
+
+let tabHeaders = document.querySelectorAll('.tab__header > span');
+let tabBodies = document.querySelectorAll('.tab__body > div');
+let currentBodyIndex = 0;
+tabHeaders.forEach((item, index) => {
+    item.addEventListener('click', function() {
+        let currentTabHeader = document.querySelector('.tab__header--active');
+        currentTabHeader.classList = '';
+        tabBodies[currentBodyIndex].classList = 'tab__body--inactive';
+        currentBodyIndex = index;
+        tabBodies[currentBodyIndex].classList = '';
+        item.classList = 'tab__header--active';
+    })
+})
+
+let btnExpand = document.querySelector('.tab__more-description');
+let tapBody = document.querySelector('.tab__body');
+btnExpand.addEventListener('click', function() {
+    if (btnExpand.classList.contains('expand--more')) {
+        btnExpand.classList.remove('expand--more');
+        btnExpand.classList.add('expand--less');
+        tapBody.style.height = "auto";
+    } else {
+        btnExpand.classList.remove('expand--less');
+        btnExpand.classList.add('expand--more');
+        tapBody.style.height = "100px";
+    }
+})
+
+let btnBuyNow = document.querySelector('.buy-now__btn');
+let btnAddToCart = document.querySelector('.add-to-cart__btn');
+let toastMsg = document.querySelector('.toast__msg');
+let toast = document.querySelector('.toast');
+
+function autoToast(msg) {
+    toastMsg.innerHTML = msg;
+    toast.classList.add('toast--show');
+    setTimeout(function() {
+        toast.classList.remove('toast--show');
+    }, 1.2 * 1000);
+}
+
+btnBuyNow.addEventListener('click', function() {
+    let checkedSize = document.querySelector('.size--checked');
+    if (checkedSize === null) {
+        autoToast("Bạn chưa chọn size!");
+    } else {
+        window.location.assign('delivery.php');
+    }
+})
+
+btnAddToCart.addEventListener('click', function() {
+    let checkedSize = document.querySelector('.size--checked');
+    if (checkedSize === null) {
+        autoToast("Bạn chưa chọn size!");
+    } else {
+        autoToast("Thêm vào giỏ hàng thành công!");
     }
 })
--- product-detail.php
+++ product-detail.php
@@ -24,6 +24,16 @@
 		</div>
 		<div class="product-detail__info">
 			<div class="product-detail__name">Áo phũ vàng cam óng ánh người đẹp</div>
+			<!-- <div class="product-detail__rating" data-href="#">
+				<div class="rating__bar">
+					<div class="rating__star star--filled"></div>
+					<div class="rating__star star--filled"></div>
+					<div class="rating__star star--filled"></div>
+					<div class="rating__star star--filled"></div>
+					<div class="rating__star star--empty"></div>
+				</div>
+				<div class="rating__text">(1 đánh giá)</div>
+			</div> -->
 			<div class="product-detail__price">
 				<span class="price--original">3.000.000đ</span>
 				<span class="price--normal">6.000.000đ</span>
@@ -42,16 +52,24 @@
 				</a>
 			</div>
 			<div class="product-detail__size">
+				<p>Size: <span></span></p>
 				<span>s</span>
 				<span>m</span>
 				<span>l</span>
 				<span>xl</span>
 			</div>
 			<div class="product-detail__quantity">
-				<label for="quantity">Số lượng: </label>
-				<input type="number" name="quantity" id="quantity" min="1" value="1">
+				<div>Số lượng: </div>
+				<div>
+					<i class="fa-solid fa-minus"></i>
+					<input type="number" name="quantity" id="quantity" min="1" value="1" readonly>
+					<i class="fa-solid fa-plus"></i>
+				</div>
 			</div>
-			<div class=""></div>
+			<div class="product-detail__purchasing">
+				<span class="buy-now__btn">Mua ngay</span>
+				<span class="add-to-cart__btn">Thêm vào giỏ hàng</span>
+			</div>
 		</div>
 		<div class="gallery--zoom">
 			<div class="controller controller__exit">
@@ -67,6 +85,37 @@
 				<i class="fa-solid fa-chevron-right"></i>
 			</div>
 		</div>
+		<div class="toast">
+			<div class="toast__icon"><i class="fa-solid fa-circle-exclamation"></i></div>
+			<div class="toast__msg"></div>
+		</div>
 	</div>
+	<div class="product-detail__description">
+			<div class="tab__header">
+				<span class="tab__header--active">Giới thiệu</span>
+				<span class="">Chi tiết sản phẩm</span>
+				<span class="">Bảo quản</span>
+			</div>
+			<div class="tab__body">
+				<div>
+					<p>Đầm được làm từ chất liệu lụa cao cấp, màu sắc và họa tiết lên rõ nét. Đầm dáng cổ V, phần tay cánh tiên được xếp từ nhiều layer chồng lên nhau tạo hiệu ứng bắt mắt. Eo đầm được nhấn cao bằng đai giả. Tà váy được phối đuôi cá hơi xòe. Dọc thân tà là khuy bọc trùng màu với đầm. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis odit, voluptatum molestias aut qui odio quidem. Doloremque cupiditate, porro quas labore minus minima ipsam sunt totam dicta hic delectus voluptas?</p>
+					<p>Thiết kế đầm bồng bềnh và tinh tế này sẽ là sự lựa chọn hoàn hảo cho chuyến thăm thú mùa hè của nàng. Họa tiết trên đầm được lên màu sáng và rõ nét trên chất liệu lụa cao cấp. Chi tiết tay cánh tiên khéo léo che đi khuyết điểm nơi bắt tay. Dáng đầm thướt tha tôn lên nét đẹp duyên dáng của nàng.</p>
+				</div>
+				<div class="tab__body--inactive">
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ex, accusantium debitis, asperiores itaque repellat expedita? Vero eligendi error, provident eveniet dignissimos, rerum facilis explicabo, molestias optio quasi aliquid nostrum!</p>
+					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt similique iste mollitia eum eos quisquam tempora, eveniet vitae quo labore dolor a provident, voluptas eius voluptates, vel perferendis ratione non.</p>
+				</div>
+				<div class="tab__body--inactive">
+					<p>Không ủi</p>
+					<p>Không mua</p>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum ipsam amet voluptatum expedita aspernatur harum quisquam eveniet vel, ratione corporis? Nisi veniam similique alias recusandae adipisci mollitia voluptates eaque quisquam!</p>
+				</div>
+			</div>
+			<div class="tab__more-description expand--more">
+				<i class="fa-solid fa-angle-up"></i>
+				<i class="fa-solid fa-angle-down"></i>
+				<div class="line"></div>
+			</div>
+		</div>
 </body>
 </html>
