f393b002bac762b89da2728244a07aee7bef2789
Phung-Anh-Quan <90770323+Phung-Anh-Quan@users.noreply.github.com>
2022-05-11 14:13:29 +0700

    Đăng ký đăng nhập

---
+++ FromDn.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset ="UTF-8">
+  <title>Đăng Nhập</title>
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+ </head>
+ <body>
+    <div class="container">
+	  <h3 class="text-uppercase">From Đăng Nhập</h3>
+	  <form action="">
+	       Username: <input type = "text" class="form-control">
+		   Password: <input type ="text" class="from-control">
+		   <input type="submit" class="btn btn-block btn-info">
+	  </form> 
+	</div>
+ </body>
