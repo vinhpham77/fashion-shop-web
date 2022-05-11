82e54f832b4a9ba0df7131c21aa399eeb6d2d63c
= <namdo4814/2gmail.com>
2022-05-11 14:00:25 +0700

    Cart

--- giohang.php
+++ giohang.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/giohangmain.css">
+    <script async src="js/giohang-sukien.js"></script>
     <title>Giỏ Hàng</title>
 </head>
 <body>
@@ -14,45 +15,45 @@
                     <tr>
 						<th>Tên Sản Phẩm</th>
                         <th></th>
-                        <th>Chiết Khấu</th>
+                        <th>Đơn giá</th>
                         <th>Số lượng</th>
                         <th>Tổng Tiền</th>
                     </tr>
                     <tr>
                         <td><img src="images/Aothun.jpg" alt="Áo Thun"></td>
                         <td><p>Áo thun Tropical <br>Màu sắc: Nâu Cà Phê <br/>Size: L</p></td>
-                        <td></td>
-                        <td><input type="number" value="1" min="1"></td>
-                        <td><p>650.000 <sup>đ</sup></p></td>
-                        <td><span>X</span></td>
+                        <td class="price-dollar"><p>650.000</p></td>
+                        <td><input class="soluong" type="number" value="1" min="1" oninput="validity.valid||(value='');"></td>
+                        <td class="thanhtien"><span>650.000 đ</span><sup></sup></td>
+                        <td><input type="button" value="X" class="close-X" onclick="xoasp(this)"></td>
                     </tr>
                     <tr>
                         <td><img src="images/dam.jpg" alt="Đầm Trắng"></td>
                         <td><p>Đầm dạ hội phối hoa hồng <br>Màu sắc: Trắng <br/>Size: M</p></td>
-                        <td></td>
-                        <td><input type="number" value="1" min="1"></td>
-                        <td><p>2.990.000đ <sup>đ</sup></p></td>
-                        <td><span>X</span></td>
+                        <td class="price-dollar"><p>2.990.000</p></td>
+                        <td><input class="soluong" type="number" value="1" min="1" oninput="validity.valid||(value='');"></td>
+                        <td class="thanhtien"><span>2.990.000 đ</span><sup></sup></td>
+                        <td><input type="button" value="X" class="close-X" onclick="xoasp(this)"></td>
 		     		</tr>
                     <tr>
                         <td><img src="images/vay.jpg" alt="Váy Jeans"></td>
                         <td><p>Zuýp jean phối túi giả <br>Màu sắc: Xanh Lơ <br/>Size: L</p></td>
-                        <td></td>
-                        <td><input type="number" value="1" min="1"></td>
-                        <td><p>1.190.000đ <sup>đ</sup></p></td>
-                        <td><span>X</span></td>
+                        <td class="price-dollar"><p>1.190.000</p></td>
+                        <td><input class="soluong" type="number" value="1" min="1" oninput="validity.valid||(value='');"></td>
+                        <td class="thanhtien"><span>1.190.000 đ</span><sup></sup></td>
+                        <td><input type="button" value="X" class="close-X" onclick="xoasp(this)"></td>
                     </tr>
 					<tr>
                         <td><img src="images/aotaydai.jpg" alt="Áo sơ mi"></td>
                         <td><p>Áo sơ mi nam dáng Slim fit <br>Màu sắc: Trắng <br/>Size: L</p></td>
-                        <td></td>
-                        <td><input type="number" value="1" min="1"></td>
-                        <td><p>1.090.000đ <sup>đ</sup></p></td>
-                        <td><span>X</span></td>
+                        <td class="price-dollar"><p>1.090.000</p></td>
+                        <td><input class="soluong" type="number" value="1" min="1" oninput="validity.valid||(value='');"></td>
+                        <td class="thanhtien"><span>1.090.000 đ</span><sup></sup></td>
+                        <td><input type="button" value="X" class="close-X" onclick="xoasp(this)"></td>
 					</tr>
                 </table>
                 <div class="cart-content-left-button">
-                    <input type="submit" value="<---  Tiếp tục mua hàng" name="muasam">
+                    <input type="submit" value="<---  Tiếp tục mua hàng" onclick="lienketMuaSam(this)">
                 </div>
             </div>
             <div class="cart-content-right">
@@ -60,26 +61,18 @@
                     <tr><th colspan="2">Tổng Tiền Giỏ Hàng</th></tr>
                     <tr>
                         <td>Tổng sản phẩm</td>
-                        <td class="sizing-right">4</td>
+                        <td class="sizing-right-number"><span>4</span></td>
                     </tr>
                     <tr>
                         <td>Tổng tiền hàng</td>
-                        <td class="sizing-right">5.920.000 <sup>đ</sup></td>
-                    </tr>
-                    <tr>
-                        <td>Thành tiền</td>
-                        <td class="sizing-right">5.920.000 <sup>đ</sup></td>
-                    </tr>
-                    <tr>
-                        <td>Tạm tính</td>
-                        <td class="sizing-right">5.920.000 <sup>đ</sup></td>
+                        <td class="sizing-right-money"><span>5.920.000</span><sup>đ</sup></td>
                     </tr>
                 </table>
-                <div class="cart-content-right-text">
-                    <p>Đơn hàng của bạn được Miễn phí ship</p>
+                <div class="cart-content-right-fee">
+                    <p>Phí ship 0<sup>đ</sup></p>
                 </div>
                 <div class="cart-content-right-button">
-                    <input type="submit" name="dathang" value="Đặt Hàng">
+                    <input type="button" value="Đặt Hàng" onclick="lienketDatHang(this)">
                 </div>
             </div>
     </div>
---
+++ js/giohang-sukien.js
@@ -0,0 +1,76 @@
+var number = document.querySelectorAll(".soluong");
+var price_money = document.querySelectorAll(".thanhtien span");
+var SumNumber = document.querySelector(".sizing-right-number span");
+var tongtien = document.querySelector(".sizing-right-money span")
+
+function xoasp(x){
+    var tr = x.parentElement.parentElement;
+    var numberone = x.parentElement.parentElement.children[3].children[0].value;
+    var tongtienmacdinh = x.parentElement.parentElement.children[4].children[0].innerHTML.replaceAll('.', '');
+    var tinhtong = tongtienmacdinh.replaceAll('đ', '');
+
+    var soluongcu = SumNumber.innerHTML;
+    var soluonghientai = soluongcu - numberone;
+
+    SumNumber.innerHTML = soluonghientai;
+    var thanhtoancu = tongtien.innerHTML.replaceAll('.', '');
+    var thanhtoanhientai = thanhtoancu - tinhtong;
+    tongtien.innerHTML = thanhtoanhientai.toLocaleString('de-DE');
+    
+    var thongbao = confirm("Bạn có chắc chắn muốn xóa không?")
+    console.log(thongbao);
+    if(thongbao==true){
+        tr.remove();
+        alert("Bạn đã xóa thành công!")
+    } 
+    number = document.querySelectorAll(".soluong");
+    price_money = document.querySelectorAll(".thanhtien span")
+}
+
+number.forEach((item, index)=>{
+    item.onchange=function(){
+        var soluong = item.value;
+        var gia = item.parentElement.previousElementSibling.children[0].innerHTML.replaceAll('.', '');
+        var giathanh;
+
+        giathanh = gia * soluong;
+        console.log(giathanh);
+        item.parentElement.nextElementSibling.children[0].innerHTML= giathanh.toLocaleString('de-DE')+' đ';
+
+        var tongSLSP = tongsl(number);
+        SumNumber.innerHTML = tongSLSP;
+
+        var tongtienmathang = tongtienhang(price_money);
+        tongtien.innerHTML = tongtienmathang.toLocaleString('de-DE');
+
+        if(soluong==''){
+            alert("Vui lòng không để trống!");
+            item.value = 1;
+            item.onchange();
+        }
+    }
+})
+function tongtienhang(e){
+    var sum = 0;
+    for(var i = 0;i< e.length;i++){
+        sum += parseInt(e[i].innerHTML.replaceAll('.', ''));
+    }
+    return sum;
+}
+
+function tongsl(x){
+    var sum = 0;
+    for(var i =0; i< x.length;i++){
+        sum += parseInt(x[i].value);
+    }return sum;
+}
+
+function lienketMuaSam(){
+    location.href="product-detail.php";
+}
+function lienketDatHang(){
+    location.href="delivery.php";
+}
+
+
+
