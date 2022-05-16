6f00dbe1719312f3d6dc49b9b8c955d8c0a0ccd3
Phung-Anh-Quan <90770323+Phung-Anh-Quan@users.noreply.github.com>
2022-05-17 00:10:30 +0700

    Update Dk.php

--- Dk.php
+++ Dk.php
@@ -3,22 +3,28 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <link rel="stylesheet" href="style/sttyle.css"/>
+
 </head>
 <body>
 
-<form method="post" action="register.php" class="form">
+<form method="post" action="register.php" class="form error">
 
 <h2>Đăng ký thành viên</h2>
 
-Username: <input type="text" name="username" value="" required>
+Username: <input type="text"id="username" name="Username" value="" required>
 
-Password: <input type="password" name="password" value="" required/>
+Password: <input type="password" id="password" name="Password" value="" required>
 
-Email: <input type="email" name="email" value="" required/>
+Full Name :<input type="text" id="name" name="Name" value="" required>
 
-Phone: <input type="text" name="phone" value="" required/>
+Current address: <input type="text" id="current" name="Current" value="" required>
+
+Email: <input type="email" id="emaill" name="Email" value="" required>
+
+Phone: <input type="text" name="sdt" id="phone" value="" required>
 
 <input type="submit" name="dangky" class="register" value="Đăng Ký"/>
  </form>
+ <script src="Xuly.js"></script>
 </body>
 </html>
