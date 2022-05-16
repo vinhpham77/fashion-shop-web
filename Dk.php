2fbf7405bf5ccc54dab908bac0d1e5d9fc5d664a
Phung-Anh-Quan <90770323+Phung-Anh-Quan@users.noreply.github.com>
2022-05-16 10:52:58 +0700

    Dk

---
+++ Dk.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="sttyle.css"/>
+</head>
+<body>
+
+<form method="post" action="register.php" class="form">
+
+<h2>Đăng ký thành viên</h2>
+
+Username: <input type="text" name="username" value="" required>
+
+Password: <input type="password" name="password" value="" required/>
+
+Email: <input type="email" name="email" value="" required/>
+
+Phone: <input type="text" name="phone" value="" required/>
+
+<input type="submit" name="dangky" class="register" value="Đăng Ký"/>
+ </form>
+</body>
+</html>
