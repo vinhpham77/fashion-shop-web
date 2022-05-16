c616cac75f2eda0f6bc3784b1d571d85570b394f
Vin <phamvanvinh8a2@gmail.com>
2022-05-16 19:33:54 +0700

    Update product-detail

--- js/product-detail.js
+++ js/product-detail.js
@@ -8,23 +8,25 @@ let prev = document.querySelector('.controller__prev');
 let currentIndex = 0;
 
 images.forEach((item, index) => {
-    item.addEventListener('click', function() {
+    item.onclick = function() {
         currentIndex = index;
         bigImg.src = images[currentIndex].src;
         let checkedProduct = document.querySelector('.product--checked');
-        checkedProduct.classList = '';
-        images[currentIndex].classList = 'product--checked checked';
-    })
+        checkedProduct.classList.remove('product--checked');
+        checkedProduct.classList.remove('checked');
+        images[currentIndex].classList.add('product--checked');
+        images[currentIndex].classList.add('checked');
+    }
 })
 
-bigImg.addEventListener('click', function() {
+bigImg.onclick = function() {
     zoom.classList.add('show');
     showBigImg();
-})
+}
 
-exit.addEventListener('click', function() {
+exit.onclick = function() {
     zoom.classList.remove('show');
-})
+}
 
 function showBigImg() {
     innerImg.src = images[currentIndex].src;
@@ -42,32 +44,34 @@ function showBigImg() {
     }
 }
 
-next.addEventListener('click', function() {
+next.onclick = function() {
     if (currentIndex < images.length - 1) {
         currentIndex++;
         showBigImg();
     }
-})
+}
 
-prev.addEventListener('click', function() {
+prev.onclick = function() {
     if (currentIndex > 0) {
         currentIndex--;
         showBigImg();
     }
-})
+}
 
 let sizes = document.querySelectorAll('.product-detail__size > span');
 let sizeName = document.querySelector('.product-detail__size > p > span');
 
 sizes.forEach((item, index) => {
-    item.addEventListener('click', function() {
+    item.onclick = function() {
         let checkedSize = document.querySelector('.size--checked');
         if (checkedSize !== null) {
-            checkedSize.classList = '';
+            checkedSize.classList.remove('size--checked');
+            checkedSize.classList.remove('checked');
         }
-        sizes[index].classList = 'size--checked checked';
+        sizes[index].classList.add('size--checked');
+        sizes[index].classList.add('checked');
         sizeName.innerHTML = sizes[index].innerHTML;
-    })
+    }
 })
 
 let plus = document.querySelector('.fa-plus');
@@ -75,73 +79,49 @@ let minus = document.querySelector('.fa-minus');
 let quantityInput = document.querySelector('#quantity');
 let quantityOnHand = 999;
 
-plus.addEventListener('click', function() {
+plus.onclick = function() {
     if (quantityInput.value < quantityOnHand) {
         quantityInput.value++;
     }
-})
+}
 
-minus.addEventListener('click', function() {
+minus.onclick = function() {
     if (quantityInput.value > 1) {
         quantityInput.value--;
     }
-})
+}
 
 let tabHeaders = document.querySelectorAll('.tab__header > span');
 let tabBodies = document.querySelectorAll('.tab__body > div');
-let currentBodyIndex = 0;
-tabHeaders.forEach((item, index) => {
-    item.addEventListener('click', function() {
-        let currentTabHeader = document.querySelector('.tab__header--active');
-        currentTabHeader.classList = '';
-        tabBodies[currentBodyIndex].classList = 'tab__body--inactive';
-        currentBodyIndex = index;
-        tabBodies[currentBodyIndex].classList = '';
-        item.classList = 'tab__header--active';
-    })
-})
 
-let btnExpand = document.querySelector('.tab__more-description');
-let tapBody = document.querySelector('.tab__body');
-btnExpand.addEventListener('click', function() {
-    if (btnExpand.classList.contains('expand--more')) {
-        btnExpand.classList.remove('expand--more');
-        btnExpand.classList.add('expand--less');
-        tapBody.style.height = "auto";
-    } else {
-        btnExpand.classList.remove('expand--less');
-        btnExpand.classList.add('expand--more');
-        tapBody.style.height = "150px";
+tabHeaders.forEach((item, index) => {
+    item.onclick = function() {
+        let currentTabHeader = document.querySelector('.tab__header--active');
+        let currentTabBody = document.querySelector('.tab__body--active');
+        currentTabHeader.classList.remove('tab__header--active');
+        item.classList.add('tab__header--active');
+        currentTabBody.classList.remove('tab__body--active');
+        tabBodies[index].classList.add('tab__body--active');
     }
 })
 
-let btnBuyNow = document.querySelector('.buy-now__btn');
-let btnAddToCart = document.querySelector('.add-to-cart__btn');
-let toastMsg = document.querySelector('.toast__msg');
-let toast = document.querySelector('.toast');
+let btnBuyNow = document.querySelector('.btn--buy-now');
+let btnAddToCart = document.querySelector('.btn--add-to-cart');
 
-function autoToast(msg) {
-    toastMsg.innerHTML = msg;
-    toast.classList.add('toast--show');
-    setTimeout(function() {
-        toast.classList.remove('toast--show');
-    }, 1.2 * 1000);
-}
-
-btnBuyNow.addEventListener('click', function() {
+btnBuyNow.onclick = function() {
     let checkedSize = document.querySelector('.size--checked');
     if (checkedSize === null) {
-        autoToast("Bạn chưa chọn size!");
+        alert("Bạn chưa chọn size!");
     } else {
         window.location.assign('delivery.php');
     }
-})
+}
 
-btnAddToCart.addEventListener('click', function() {
+btnAddToCart.onclick = function() {
     let checkedSize = document.querySelector('.size--checked');
     if (checkedSize === null) {
-        autoToast("Bạn chưa chọn size!");
+        alert("Bạn chưa chọn size!");
     } else {
-        autoToast("Thêm vào giỏ hàng thành công!");
+        alert("Thêm vào giỏ hàng thành công!");
     }
-})
+}
--- product-detail.php
+++ product-detail.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Product Detail</title>
-	<link rel="stylesheet" href="style/reset.css">
+	<link rel="stylesheet" href="style/base.css">
 	<link rel="stylesheet" href="style/product-detail.css">
 	<link rel="stylesheet" href="fontawesome/css/all.min.css">
 	<script async type="text/javascript" src="js/product-detail.js"></script>
@@ -12,60 +12,48 @@
 <body>
 	<div class="product__detail">
 		<div class="product-detail__gallery">
-			<div class="gallery--big" id="gallery--big">
-				<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/21/0419924ccc06d6fbd9c5eae7e8a0afe6.JPG" alt="Ảnh sản phẩm"> 
+			<div class="gallery--big bordered__object" id="gallery--big">
+				<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/21/0419924ccc06d6fbd9c5eae7e8a0afe6.JPG" alt="Ảnh sản phẩm" class="bordered__object"> 
 			</div>
 			<div class="gallery--small">
-				<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/21/0419924ccc06d6fbd9c5eae7e8a0afe6.JPG" alt="Ảnh sản phẩm" class="product--checked checked">
-				<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/21/21c048c5d895eeb3856cdafe789b20f9.JPG" alt="Ảnh sản phẩm">
-				<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/21/5e0379dbf1c0dcb54b8bd8e20d96cdb6.JPG" alt="Ảnh sản phẩm">
-				<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/21/6a5a99013687159b6466c995820a23bb.JPG" alt="Ảnh sản phẩm">
+				<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/21/0419924ccc06d6fbd9c5eae7e8a0afe6.JPG" alt="Ảnh sản phẩm" class="option product--checked checked bordered__object">
+				<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/21/21c048c5d895eeb3856cdafe789b20f9.JPG" alt="Ảnh sản phẩm" class="option bordered__object">
+				<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/21/5e0379dbf1c0dcb54b8bd8e20d96cdb6.JPG" alt="Ảnh sản phẩm" class="option bordered__object">
+				<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/21/6a5a99013687159b6466c995820a23bb.JPG" alt="Ảnh sản phẩm" class="option bordered__object">
 			</div>
 		</div>
 		<div class="product-detail__info">
-			<div class="product-detail__name">Áo phũ vàng cam óng ánh người đẹp</div>
+			<h1 class="product-detail__name">Áo phũ vàng cam óng ánh</h1>
 			<div class="product-detail__price">
 				<span class="price--original">3.000.000đ</span>
 				<span class="price--normal">6.000.000đ</span>
 				<span class="price--discount">-50%</span>
 			</div>
-			<div class="product-detail__colors">
-				<p>Màu sắc: <span>Vàng cam óng ánh</span></p>
-				<a href="">
-					<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/21/0419924ccc06d6fbd9c5eae7e8a0afe6.JPG" class="color--checked checked">
-				</a>
-				<a href="">
-					<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/27/94457894fa5d449111a63f732ff29fb6.JPG">
-				</a>
-				<a href="">
-					<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/06/7a886802addb7aad56499ea9a42c0ca4.JPG">
-				</a>
-			</div>
 			<div class="product-detail__size">
 				<p>Size: <span></span></p>
-				<span>s</span>
-				<span>m</span>
-				<span>l</span>
-				<span>xl</span>
+				<span class="bordered__object option">s</span>
+				<span class="bordered__object option">m</span>
+				<span class="bordered__object option">l</span>
+				<span class="bordered__object option">xl</span>
 			</div>
 			<div class="product-detail__quantity">
 				<div>Số lượng: </div>
-				<div>
+				<div class="bordered__object">
 					<i class="fa-solid fa-minus"></i>
 					<input type="number" name="quantity" id="quantity" min="1" value="1" readonly>
 					<i class="fa-solid fa-plus"></i>
 				</div>
 			</div>
 			<div class="product-detail__purchasing">
-				<span class="buy-now__btn">Mua ngay</span>
-				<span class="add-to-cart__btn">Thêm vào giỏ hàng</span>
+				<span class="btn--buy-now bordered__object">Mua ngay</span>
+				<span class="btn--add-to-cart bordered__object">Thêm vào giỏ hàng</span>
 			</div>
 		</div>
 		<div class="gallery--zoom">
 			<div class="controller controller__exit">
 				<i class="fa-solid fa-x"></i>
 			</div>
-			<div class="gallery__inner">
+			<div class="gallery__inner bordered__object">
 				<img src="https://pubcdn.ivymoda.com/files/product/thumab/1400/2022/04/21/0419924ccc06d6fbd9c5eae7e8a0afe6.JPG" alt="Ảnh sản phẩm" alt="">
 			</div>
 			<div class="controller controller__prev">
@@ -75,10 +63,6 @@
 				<i class="fa-solid fa-chevron-right"></i>
 			</div>
 		</div>
-		<div class="toast">
-			<div class="toast__icon"><i class="fa-solid fa-circle-exclamation"></i></div>
-			<div class="toast__msg"></div>
-		</div>
 	</div>
 	<div class="product__description">
 			<div class="tab__header">
@@ -87,25 +71,21 @@
 				<span class="">Bảo quản</span>
 			</div>
 			<div class="tab__body">
-				<div>
+				<div class="tab__body--active">
 					<p>Đầm được làm từ chất liệu lụa cao cấp, màu sắc và họa tiết lên rõ nét. Đầm dáng cổ V, phần tay cánh tiên được xếp từ nhiều layer chồng lên nhau tạo hiệu ứng bắt mắt. Eo đầm được nhấn cao bằng đai giả. Tà váy được phối đuôi cá hơi xòe. Dọc thân tà là khuy bọc trùng màu với đầm. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis odit, voluptatum molestias aut qui odio quidem. Doloremque cupiditate, porro quas labore minus minima ipsam sunt totam dicta hic delectus voluptas?</p>
 					<p>Thiết kế đầm bồng bềnh và tinh tế này sẽ là sự lựa chọn hoàn hảo cho chuyến thăm thú mùa hè của nàng. Họa tiết trên đầm được lên màu sáng và rõ nét trên chất liệu lụa cao cấp. Chi tiết tay cánh tiên khéo léo che đi khuyết điểm nơi bắt tay. Dáng đầm thướt tha tôn lên nét đẹp duyên dáng của nàng.</p>
 				</div>
-				<div class="tab__body--inactive">
+				<div>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ex, accusantium debitis, asperiores itaque repellat expedita? Vero eligendi error, provident eveniet dignissimos, rerum facilis explicabo, molestias optio quasi aliquid nostrum!</p>
 					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt similique iste mollitia eum eos quisquam tempora, eveniet vitae quo labore dolor a provident, voluptas eius voluptates, vel perferendis ratione non.</p>
 				</div>
-				<div class="tab__body--inactive">
+				<div>
 					<p>Không ủi</p>
 					<p>Không mua</p>
 					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum ipsam amet voluptatum expedita aspernatur harum quisquam eveniet vel, ratione corporis? Nisi veniam similique alias recusandae adipisci mollitia voluptates eaque quisquam!</p>
 				</div>
 			</div>
-			<div class="tab__more-description expand--more">
-				<i class="fa-solid fa-angle-up"></i>
-				<i class="fa-solid fa-angle-down"></i>
-				<div class="line"></div>
-			</div>
+			<div class="line"></div>
 		</div>
 </body>
 </html>
