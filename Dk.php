74b231f6499d41fd3dfc7a3374cf1c7bf07dd36e
Phung-Anh-Quan <90770323+Phung-Anh-Quan@users.noreply.github.com>
2022-05-17 19:59:19 +0700

    Update Dk.php

--- Dk.php
+++ Dk.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link rel="stylesheet" href="sttyle.css"/>
+<link rel="stylesheet" href="style/sttyle.css"/>
 
 </head>
 <body>
