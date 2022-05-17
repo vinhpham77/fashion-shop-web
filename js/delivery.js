8c9ef253afb8bbdab9e9603fad7ce5a19a443a22
nguyenhongban0210 <104150893+nguyenhongban0210@users.noreply.github.com>
2022-05-17 21:31:54 +0700

    GiaoHang

---
+++ js/delivery.js
@@ -0,0 +1,12 @@
+function trangdangnhap(){
+    location.href = "FromDn.php";
+}
+function trangdangky(){
+    location.href = "Dk.php";
+}
+function quaylaigiohang(){
+    location.href = "giohang.php";
+}
+function trangthanhtoan(){
+    location.href = "banhang.php";
+}
