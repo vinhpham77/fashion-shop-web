a8adbd55315d882256f86c01593583ba3e9ac8c6
GiangNam77 <namdo4814@gmail.com>
2022-05-18 15:13:22 +0700

    quanlycsdl

--- database/fashion-shop (1).sql
+++ database/fashion-shop (1).sql
@@ -43,7 +43,7 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`username`, `password`, `account_type`, `fullname`, `current_address`, `email`, `phone_number`, `shipping_address`) VALUES
-('AliceMr', '123Alice', b'0', '', '46-Phan Đình Phùng-Phạm Kim Đồng-Đà Lạt', 'Alice1111@gmail.com', '0563238915', '46-Phan Đình Phùng-Phạm Kim Đồng-Đà Lạt'),
+('AliceMr', '123Alice', b'0', 'AliceHan', '46-Phan Đình Phùng-Phạm Kim Đồng-Đà Lạt', 'Alice1111@gmail.com', '0563238915', '46-Phan Đình Phùng-Phạm Kim Đồng-Đà Lạt'),
 ('Henlee', '123Henlee', b'0', 'Helenka', '02-Định Văn Của-Xuân Vinh-Rạch Giá', 'Henlee123@gmail.com', '0834554432', 'KBang-Chư Pưng-Gia Lai'),
 ('JamesJon', '123James', b'0', 'Jon Văn James', '61-Lý Thái Tổ-Xuân Diệu-Quy Nhơn', 'James222@gmail.com', '0905232261', '61-Lý Thái Tổ-Xuân Diệu-Quy Nhơn'),
 ('ThompsonRobert', '123Robert', b'0', ' Robert Thompson', ' Phụng Hoài Hảo-Hoài Nhơn-Bình Định', ' RobertThompson000@gmail.com', '0394778001', '53-Nguyễn Đình Thụ-Nguyễn Văn Cừ-Hồ Chí Minh'),
@@ -133,7 +133,54 @@ CREATE TABLE `description` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- --------------------------------------------------------
-
+INSERT INTO `description`(`prod_id`, `introduction`, `detail`, `maintenance`) 
+VALUES (1,'<p>Sơ mi nam tay ngắn. Trên nền vải denim được đánh bạc màu giả. Phía trước có túi ngực. Ống tay gập gấu khoảng 2-3cm. Khuy cài cùng màu với nền vải.</p>
+<p>Thiết kế sơ mi denim mang đến vẻ năng động và trẻ trung. Sắc đen ngả bạc giúp vẻ ngoài của các chàng trai thêm phần phong trần, mạnh mẽ. Thiết kế chú trọng vào 
+tính cơ bản nên phù hợp để chàng diện vào nhiều dịp khác nhau.</p>','Cổ Áo: cổ đức<br>Tay Áo: tay cộc<br>Kiểu Áo: Regular fit<br>Độ dài: dài thường<br>Họa Tiết: trơn',
+'* Vải dệt kim : sau khi giặt sản phẩm phải được phơi ngang tránh bai dãn.')
+INSERT INTO `description`(`prod_id`, `introduction`, `detail`, `maintenance`) 
+VALUES (2,'<p>Áo sơ mi cổ đức, tay ngắn. Cài bằng hàng khuy phía trước. Dáng regular kết hợp với chất liệu lụa cao cấp giúp bạn mặc thoải mái và dễ chịu. MIx cùng quần jeans màu đen, sooc để có set đồ ăn ý nhất nhé!</p>',
+'Cổ Áo: khác<br>Tay Áo: tay ngắn<br>Kiểu Áo: Regular<br>Độ dài: dài thường<br>Họa Tiết: in hình','* Vải dệt kim : sau khi giặt sản phẩm phải được phơi ngang tránh bai dãn.')
+INSERT INTO `description`(`prod_id`, `introduction`, `detail`, `maintenance`) 
+VALUES (3,'<p>Áo sơ mi dáng slim fit với cổ đức và ống tay dài. Họa tiết kẻ mờ. Độ dài vừa phải. Có viền ngoài để giấu khuy cài.</p>
+<p>Những chiếc áo sơ mi kẻ luôn được lòng các quý ông công sở. Thiết kế chú trọng đến sự tối giản sẽ tôn lên vẻ ngoài phong độ, chỉn chu của người mặc. Dáng slim fit ôm nhẹ phù hợp để bạn mặc khi diện vest và quần Âu.',
+'Cổ Áo: cổ đức<br>Tay Áo: tay dài<br>Kiểu Áo: Slim fit<br>Độ dài: dài thường<br>Họa Tiết: kẻ',
+'* Vải dệt kim : sau khi giặt sản phẩm phải được phơi ngang tránh bai dãn.')
+INSERT INTO `description`(`prod_id`, `introduction`, `detail`, `maintenance`) 
+VALUES (4,'<p>Áo sơ mi cổ đức phối màu hình tam giác. Tay dài bo gấu và 2 khuy cài đính kèm. Vạt áo hình phía trước. Cài bằng hàng khuy ẩn tà phía trước.</p>
+<p>Kiểu dáng Slim fit thiết kế form ôm nhẹ, tôn đường nét của cơ thể. Tông màu lịch lãm, dễ phối nhiều trang phục như quần jean, short, quần tây... Áo có thể sơ vin hoặc thả suông nhờ tà lượn thời trang.</p>',
+'Cổ Áo: cổ đức<br>Tay Áo: tay dài<br>Kiểu Áo: Slim fit<br>Độ dài: dài thường, Oversize<br>Họa Tiết: trơn',
+'* Vải dệt kim : sau khi giặt sản phẩm phải được phơi ngang tránh bai dãn.')
+INSERT INTO `description`(`prod_id`, `introduction`, `detail`, `maintenance`) 
+VALUES (5,'<p>Áo sơ mi cổ đức. Tay dài bo gấu và 2 khuy cài đính kèm. 1 túi vuông trước ngực. Cài bằng hàng khuy phía trước. Vải họa tiết tinh tế.</p>
+<p>Kiểu dáng Slim fit thiết kế form ôm nhẹ, tôn đường nét của cơ thể. Tông màu lịch lãm, dễ phối nhiều trang phục như quần jean, short, quần tây... Áo có thể sơ vin hoặc thả suông nhờ tà lượn thời trang.</p>',
+'Cổ Áo: cổ đức<br>Tay Áo: tay dài<br>Kiểu Áo: Slim fit<br>Độ dài: dài thường<br>Họa Tiết: khác',
+'* Vải dệt kim : sau khi giặt sản phẩm phải được phơi ngang tránh bai dãn.')
+INSERT INTO `description`(`prod_id`, `introduction`, `detail`, `maintenance`) 
+VALUES (6,'<p>Zuýp chữ A 2 lớp dáng trên gối. Có 1 móc kim loại tạo kiểu phía trước. 2 viền túi giả 2 bên. Cài bằng khóa ẩn sau lưng.</p>
+<p>gợi ý cho bạn cứ mix theo combo ton-sur-ton với áo vest đồng bộ MS 67M7076 sẽ có vẻ ngoài trông thật sành điệu, tự tin ghi điểm tuyệt đối trong mắt người đối diện. Muốn làm mới phong cách thời trang công sở của mình, nhất định phải có ít nhất một kiểu chân váy như thế này trong tủ đồ của mình nhé!</p>',
+'Kiểu dáng: chữ A<br>Độ dài: trên gối<br>Chất liệu: Tuysi',
+'* Vải dệt kim : sau khi giặt sản phẩm phải được phơi ngang tránh bai dãn.')
+INSERT INTO `description`(`prod_id`, `introduction`, `detail`, `maintenance`) 
+VALUES (7,'<p>Zuýp chữ A 2 lớp. Mặt trước dưới gấu thêu chữ và đính hoa nổi khác màu tạo nổi bật. Cài bằng khóa kéo ẩn sau lưng.</p>
+<p>Với chiều dài trên gối cùng chất liệu Tuysi cao cấp giúp nàng trở nên trẻ trung và năng động hơn. Mix cùng áo blazer, áo sơ mi kiểu, áo thun basic là bạn có thể tự tin bước ra ngoài rồi!</p>',
+'Kiểu dáng: chữ A<br>Độ dài: trên gối<br>Họa tiết: trơn<br>Chất liệu: Tuysi',
+'* Vải dệt kim : sau khi giặt sản phẩm phải được phơi ngang tránh bai dãn.')
+INSERT INTO `description`(`prod_id`, `introduction`, `detail`, `maintenance`) 
+VALUES (8,'<p>Zuýp chữ A  2 lớp. Mặt trước có hàng khuy bọc vải kiểu chéo trang trí. Xẻ tạo kiểu dưới gấu. </p>
+<p>Chất liệu Tuysi không quá dày cũng không quá mỏng tạo form dáng cứng cáp, không xù lông đặc biệt không nhăn, dễ dàng tạo sự thoải mái, không gò bó và mang đến nét đẹp năng động, trẻ trung cho người mặc. Chân váy chữ A công sở cũng ghi điểm nhờ sự “thân thiện” có thể kết hợp cùng áo sơ mi, áo thun… đầy ấn tượng.</p>',
+'Kiểu dáng: chữ A<br>Độ dài: trên gối<br>Họa tiết: trơn<br>Chất liệu: Tuysi',
+'* Vải dệt kim : sau khi giặt sản phẩm phải được phơi ngang tránh bai dãn.')
+INSERT INTO `description`(`prod_id`, `introduction`, `detail`, `maintenance`) 
+VALUES (9,'<p>Zuýp chữ A 2 lớp cạp có đai liền và đỉa ở giữa. Phom chữ A basic. Trang trí bằng các đường chần chỉ nổi màu trắng tạo điểm nhấn.</p>
+<p>Sử dụng chất vải Tuysi  bên trong có lớp lót dính liền tạo cho nàng cảm giác thoải mái khi diện thiết kế này. Dáng chữ A dễ phối hợp với nhiều kiểu áo, và phụ kiện giúp nàng trở nên năng động và tự tin hơn.</p>',
+'Kiểu dáng: chữ A<br>Độ dài: trên gối<br>Họa tiết: trơn<br>Chất liệu: Tuysi',
+'* Vải dệt kim : sau khi giặt sản phẩm phải được phơi ngang tránh bai dãn.')
+INSERT INTO `description`(`prod_id`, `introduction`, `detail`, `maintenance`) 
+VALUES (10,'<p>Zuýp chữ A dáng dài ngang bắp. Cạp cao. Mặt trước trang trí hàng khuy làm điểm nhấn. Cài bằng khóa kéo ẩn phía sau. Có thể mix cùng áo croptop đồng bộ.</p>
+<p>Chất liệu vải Thô xước mang lại cảm giác thoáng mát, thoải mái cho người mặc bởi khả năng hút ẩm cao, thấm hút tốt.</p>',
+'Kiểu dáng: chữ A<br>Độ dài: ngang bắp<br>Họa tiết: trơn<br>Chất liệu: thô',
+'* Vải dệt kim : sau khi giặt sản phẩm phải được phơi ngang tránh bai dãn.')
 --
 -- Cấu trúc bảng cho bảng `order`
 --
@@ -166,9 +213,28 @@ CREATE TABLE `product` (
 --
 -- Đang đổ dữ liệu cho bảng `product`
 --
-
+/*Áo sơ mi nam*/
 INSERT INTO `product` (`prod_id`, `cate_id`, `prod_name`, `price`, `discount`, `quantity`, `date_added`) VALUES
-(1, 5, 'ÁO SƠ MI LỤA ĐŨI', 1090000, '10.00', 200, '2022-05-17 13:19:02');
+(1, 5, 'ÁO SƠ MI DENIM', 890000, '10.00', 200, '2022-05-17 13:19:02');
+INSERT INTO `product` (`prod_id`, `cate_id`, `prod_name`, `price`, `discount`, `quantity`, `date_added`) VALUES
+(2, 5, 'ÁO SƠ MI NAM HỌA TIẾT', 1090000, '20.00', 90, '2022-05-18 11:33:39');
+INSERT INTO `product` (`prod_id`, `cate_id`, `prod_name`, `price`, `discount`, `quantity`, `date_added`) VALUES
+(3, 5, 'ÁO SƠ MI KẺ MỜ', 1190000, '0', 199, '2022-05-18 1:26:14');
+INSERT INTO `product` (`prod_id`, `cate_id`, `prod_name`, `price`, `discount`, `quantity`, `date_added`) VALUES
+(4, 5, 'ÁO SƠ MI CỔ PHỐI MÀU', 1099000, '10.00', 70, '2022-05-18 1:27:09');
+INSERT INTO `product` (`prod_id`, `cate_id`, `prod_name`, `price`, `discount`, `quantity`, `date_added`) VALUES
+(5, 5, 'ÁO SƠ MI TAY DÀI TÚI VUÔNG', 990000, '30.00', 30, '2022-05-18 1:32:01');
+/*chân váy chữ A*/
+INSERT INTO `product` (`prod_id`, `cate_id`, `prod_name`, `price`, `discount`, `quantity`, `date_added`) VALUES
+(6, 10, 'CHÂN VÁY CHỮ A 2 LỚP', 1090000, '30.00', 120, '2022-05-18 10:35:20');
+INSERT INTO `product` (`prod_id`, `cate_id`, `prod_name`, `price`, `discount`, `quantity`, `date_added`) VALUES
+(7, 10, 'CHÂN VÁY CHỮ A HOA NỔI', 990000, '0', 20, '2022-05-18 11:05:09');
+INSERT INTO `product` (`prod_id`, `cate_id`, `prod_name`, `price`, `discount`, `quantity`, `date_added`) VALUES
+(8, 10, 'CHÂN VÁY CHỮ A PHỐI NÚT', 790000, '30.00', 50, '2022-05-18 11:09:05');
+INSERT INTO `product` (`prod_id`, `cate_id`, `prod_name`, `price`, `discount`, `quantity`, `date_added`) VALUES
+(9, 10, 'CHÂN VÁY CHỮ A CHẦN CHỈ NỔI', 890000, '0', 110, '2022-05-18 11:12:20');
+INSERT INTO `product` (`prod_id`, `cate_id`, `prod_name`, `price`, `discount`, `quantity`, `date_added`) VALUES
+(10, 10, 'CHÂN VÁY CHỮ A DÁNG DÀI', 990000, '10.00', 160, '2022-05-18 11:20:59');
 
 -- --------------------------------------------------------
 
@@ -188,7 +254,16 @@ CREATE TABLE `size` (
 --
 -- Chỉ mục cho các bảng đã đổ
 --
-
+INSERT INTO `size`(`prod_id`, `s`, `m`, `x`, `l`, `xxl`) VALUES (1, 50, 70, 40, 30, 10)/*số lượng 200*/
+INSERT INTO `size`(`prod_id`, `s`, `m`, `x`, `l`, `xxl`) VALUES (2, 25, 15, 20, 20, 10)/*số lượng 90*/
+INSERT INTO `size`(`prod_id`, `s`, `m`, `x`, `l`, `xxl`) VALUES (3, 80, 50, 40, 10, 19)/*số lượng 199*/
+INSERT INTO `size`(`prod_id`, `s`, `m`, `x`, `l`, `xxl`) VALUES (4, 30, 10, 10, 10, 10)/*số lượng 70*/
+INSERT INTO `size`(`prod_id`, `s`, `m`, `x`, `l`, `xxl`) VALUES (5, 10, 5, 5, 5, 5)/*số lượng 30*/
+INSERT INTO `size`(`prod_id`, `s`, `m`, `x`, `l`, `xxl`) VALUES (6, 20, 30, 40, 10, 20)/*số lượng 120*/
+INSERT INTO `size`(`prod_id`, `s`, `m`, `x`, `l`, `xxl`) VALUES (7, 4, 6, 2, 6, 2)/*số lượng 20*/
+INSERT INTO `size`(`prod_id`, `s`, `m`, `x`, `l`, `xxl`) VALUES (8, 10, 5, 5, 20, 10)/*số lượng 50*/
+INSERT INTO `size`(`prod_id`, `s`, `m`, `x`, `l`, `xxl`) VALUES (9, 40, 20, 40, 5, 5)/*số lượng 110*/
+INSERT INTO `size`(`prod_id`, `s`, `m`, `x`, `l`, `xxl`) VALUES (10, 20, 50, 30, 30, 30)/*số lượng 160*/
 --
 -- Chỉ mục cho bảng `account`
 --
