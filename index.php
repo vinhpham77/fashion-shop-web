b5cf5ac27c76aab8a19ec09b039ff924db695caa
Vin <phamvanvinh8a2@gmail.com>
2022-05-22 16:55:44 +0700

    Update index

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fashion_Shop</title> 
-    <link async rel="stylesheet" href="./style/home.css">
-    <script src="./js/home1.js"></script>
+    <link rel="stylesheet" href="./style/home.css">
+    <script async src="./js/home1.js"></script>
     <link rel="stylesheet" href="./fontawesome/css/all.min.css">
 </head>
 <body>
