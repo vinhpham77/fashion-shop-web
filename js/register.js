18b3b9d35158fa85e94a992abcbec748c1840761
Vin <phamvanvinh8a2@gmail.com>
2022-05-23 23:36:31 +0700

    Update register

--- js/register.js
+++ js/register.js
@@ -1,69 +1,108 @@
-// function Validator(options){
-//    let formElement = document.querySelector(options.form);
-//       console.log(options.rule);
-//    if(formElement){
-//        options.rule.forEach(function(rule){
-//            let inputElement = formElement.querySelector(rule.selector);
-//            if(inputElement){
-//                inputElement.onblur = function(){
-//                 let errorMessage=rule.test(in);
-//                }
-//            }
-//        });
-//    }
-// }
-// // Validator.isrequired = function(){
+let enterableElements = document.querySelectorAll('.register input, .register textarea');
+const USERNAME_FORMAT = /^\w{5,20}$/;
+const PASSWORD_FORMAT = USERNAME_FORMAT;
+const PHONE_NUMBER_FORMAT = /^0\d{9}$/;
+const EMAIL_FORMAT = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const USERNAME_MESSAGE = 'Bao gồm 5-20 kí tự và chỉ chứa A-Z, a-z, 0-9';
+const PASSWORD_MESSAGE = USERNAME_MESSAGE;
+const REPASSWORD_MESSAGE = 'Không khớp với mật khẩu ở trên';
+const PHONE_NUMBER_MESSAGE = 'Bao gồm 10 chữ số và bắt đầu bằng số 0';
+const EMAIL_MESSAGE = 'Không đúng định dạng email';
 
-// // }
+enterableElements.forEach(function(item) {
+    item.onblur = function() {
+        return validateElement(item, getFormat(item));
+    }
+})
 
-function getValue(id){
-    return document.getElementById(input).value.trim();
+function getFormat(element) {
+    switch (element.id) {
+        case 'username':
+            return USERNAME_FORMAT;
+        case 'password':
+            return PASSWORD_FORMAT;
+        case 'phone-number':
+            return PHONE_NUMBER_FORMAT;
+        case 'email':
+            return EMAIL_FORMAT;
+        default:
+            return '';
+    }
 }
 
-
-function showError(id, message){
-    document.getElementById(input + '_error').innerHTML = message;
+function getMessage(element) {
+    switch (element.id) {
+        case 'username':
+            return USERNAME_MESSAGE;
+        case 'password':
+            return PASSWORD_MESSAGE;
+        case 'phone-number':
+            return PHONE_NUMBER_MESSAGE;
+        case 'email':
+            return EMAIL_MESSAGE;
+        default:
+            return '';
+    }
 }
 
+function getFormMessage(element) {
+    let formGroup = element.parentElement;
+    return formGroup.querySelector('.form-message');
+}
 
- 
-function validate()
-{
+function showMessage(element, message) {
+    let formMessage = getFormMessage(element);
+    formMessage.innerHTML = message;
+    element.classList.add('invalid');
+}
+
+function hideMessage(element) {
+    let formMessage = getFormMessage(element);
+    formMessage.innerHTML = '';
+    element.classList.remove('invalid');
+}
+
+function isEmpty(element) {
+    return element.value ? false : true;
+}
+
+function validateElement(element, format) {
+    element.value = element.value.trim();
+    if (isEmpty(element)) {
+        showMessage(element, "Vui lòng không để trống ô này");
+        return false;
+    }
+
+    if (element.id === 're-password') {
+        let password = document.querySelector('#password');
+        if (password.value !== element.value) {
+            showMessage(element, REPASSWORD_MESSAGE);
+            return false;
+        } else {
+            hideMessage(element);
+            return true;
+        }
+    } else {
+        let format = getFormat(element);
+        let value = element.value;
+
+        if (format && !format.test(value)) {
+            showMessage(element, getMessage(element));
+            return false;
+        } else {
+            hideMessage(element);
+            return true;
+        }
+    }
+}
+
+function validateForm() {
     let isValid = true;
-
-    let username = getValue('username');
-    let usernameFormat = /^\w{5,20}$/;
-    if (!usernameFormat.test(username)){
-        isValid = false;
-        showError('username', 'Bao gồm 5-20 kí tự và chỉ chứa A-Z, a-z, 0-9');
-    }
-
-    let password = getValue('password');
-    let passwordFormat = usernameFormat;
-    if (!passwordFormat.test(username)){
-        isValid = false;
-        showError('password', 'Bao gồm 5-20 kí tự và chỉ chứa A-Z, a-z, 0-9');
-    }
-
-    let rePassword = getValue('re-password');
-    if (rePassword !== password) {
-        isValid = false;
-        showError('re-password', 'Không khớp với mật khẩu ở trên');
-    }
-
-    let phoneNumber = getValue('phone-number');
-    let phoneNumberFormat = /^0\d{9}$/;
-    if (!phoneNumberFormat.test(phone)){
-        isValid = false;
-        showError('phone-number', 'Bao gồm 10 chữ số và bắt đầu bằng số 0');
-    }
-    
-    let email = getValue('email');
-    let mailFormat = /^[\w.!#$%&'*+/=?^_`{|}~-]+@[\w-]+(?:\.[\w-]+)+$/;
-    if (!mailFormat.test(email)){
-        isValid = false;
-        showError('email', 'Không đúng định dạng email');
-    }
+    enterableElements.forEach(function(item) {
+        if (!validateElement(item, getFormat(item))) {
+            isValid = false;
+        }
+    });
 
     return isValid;
 }
--- register.php
+++ register.php
@@ -4,38 +4,60 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" href="style/base.css">
     <link rel="stylesheet" href="style/register.css"/>
-    <!-- <script async src="js/register.js"></script> -->
+    <script defer src="js/register.js"></script>
 </head>
 <body>
-    <form method="post" action="" class="register">
+    <form method="post" action="" class="register" onsubmit="return validateForm();">
         <div class="account__detail">
             <h2>Tài khoản</h2>
-            <label for="username">Tên tài khoản: </label>
-            <input type="text" name="username" id="username" pattern="^\w{5,20}$" title="Bao gồm 5-20 kí tự, chỉ chứa A-Z, a-z, 0-9">
-            <span class="error" id="username_error"></span>
-            <label for="password">Mật khẩu: </label>
-            <input type="password" name="password" id="password" pattern="^\w{5,20}$" title="Bao gồm 5-20 kí tự, chỉ chứa A-Z, a-z, 0-9">
-            <span class="error" id="password_error"></span>
-            <label for="re-password">Nhập lại mật khẩu: </label>
-            <input type="password" name="re-password" id="re-password">
-            <span class="error" id="re-password_error"></span>
+            <div class="form-group">
+                <label for="username">Tên tài khoản: </label>
+                <input type="text" name="username" id="username" title="Bao gồm 5-20 kí tự, chỉ chứa A-Z, a-z, 0-9">
+                <span class="form-message"></span>
+            </div>
+            <div class="form-group">
+                <label for="password">Mật khẩu: </label>
+                <input type="password" name="password" id="password" title="Bao gồm 5-20 kí tự, chỉ chứa A-Z, a-z, 0-9">
+                <span class="form-message"></span>
+            </div>
+            <div class="form-group">
+                <label for="re-password">Nhập lại mật khẩu: </label>
+                <input type="password" name="re-password" id="re-password">
+                <span class="form-message"></span>
+            </div>
         </div>
         <div class="user__detail">
             <h2>Thông tin</h2>
-            <label for="fullname">Họ tên: </label>
-            <input type="text" name="fullname" id="fullname" required>
-            <label for="current-address">Chỗ ở hiện tại: </label>
-            <textarea name="current-address" id="current-address" required></textarea>
-            <label for="email">Email: </label>
-            <input type="email" name="email" id="email" required>
-            <span class="error" id="email_error"></span>
-            <label for="phone-number">Số điện thoại: </label>
-            <input type="tel" name="phone-number" id="phone-number" pattern="^0\d{9}$" title=
-            "Số điện thoại bao gồm 10 chữ số bắt đầu bằng số 0" required>
-            <span class="error" id="phone-number_error"></span>
-            <label for="shipping-address">Địa chỉ giao hàng: </label>
-            <textarea name="shipping-address" id="shipping-address" required></textarea>
-            <button type="submit" name="btn-register" id="btn-register"/>Đăng Ký</button>
+            <div class="form-group">
+                <label for="fullname">Họ tên: </label>
+                <input type="text" name="fullname" id="fullname">
+                <span class="form-message"></span>
+            </div>
+            <div class="form-group">
+                <label for="current-address">Chỗ ở hiện tại: </label>
+                <textarea name="current-address" id="current-address"></textarea>
+                <span class="form-message"></span>
+            </div>
+            <div class="form-group">
+                <label for="email">Email: </label>
+                <input type="email" name="email" id="email">
+                <span class="form-message"></span>
+            </div>
+            <div class="form-group">
+                <label for="phone-number">Số điện thoại: </label>
+                <input type="tel" name="phone-number" id="phone-number" title=
+                "Số điện thoại bao gồm 10 chữ số bắt đầu bằng số 0">
+                <span class="form-message"></span>
+            </div>
+            <span class="form-message"></span>
+            <div class="form-group">
+                <label for="shipping-address">Địa chỉ giao hàng: </label>
+                <textarea name="shipping-address" id="shipping-address"></textarea>
+                <span class="form-message"></span>
+            </div>
+            <div class="form-group">
+                <button type="submit" name="btn-register" id="btn-register"/>Đăng Ký</button>
+            </div>
         </div>
     </form>
 </body>
