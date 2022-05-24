4f1532e153515c97e4cff8f81974cc80847fc5d7
Vin <phamvanvinh8a2@gmail.com>
2022-05-24 16:09:17 +0700

    Update connect_db.php

--- widget/connect_db.php
+++ widget/connect_db.php
@@ -1,4 +1,4 @@
 <?php 
-    $conn = new mysqli('localhost', 'root', '', '6aesn');
+    $conn = new mysqli('localhost', 'root', '', '6aesn') or die('Không thể kết nối cơ sở dữ liệu');
     $conn->set_charset('utf8');
 ?>
