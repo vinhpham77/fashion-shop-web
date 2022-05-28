16388de0c3bb699db183d966e5f56db481222c54
GiangNam77 <namdo4814@gmail.com>
2022-05-28 07:35:52 +0700

    DangNhap

--- FormDangNhap.php
+++ FormDangNhap.php
@@ -26,13 +26,8 @@
 <body>
     <form method="post" action="" class="dangnhap">
         <h2>Đăng Nhập</h2>
-<<<<<<< HEAD
-        <div class="user">Username: <input type="text" name="username" value=""></div>
-        <div class="pass">Password: <input type="password" name="password" value=""/></div>
-=======
         <div class="user">Username: <input type="text" name="username" value="<?php echo $username; ?>" required></div>
         <div class="pass">Password: <input type="password" name="password" value="<?php echo $password; ?>" required/></div>
->>>>>>> 601bfec6c57d02de38e790c022e8f640534f007f
         <div class="btdangnhap"><input type="submit" name="dangky" class="login" value="Đăng Nhập"/></div>
         <div class="btdangky"><input type="button" name="dangky" class="register" value="Đăng Ký" onclick="window.location.href='register.php';"></div>
     </form>
