eb1e1d06fa3ada0a3cfe93b41f938d131d45f929
Vin <phamvanvinh8a2@gmail.com>
2022-06-03 14:11:08 +0700

    Update doimk.php

--- doimk.php
+++ doimk.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="style/doimk.css"/>
 <?php
 require_once('connect_db.php'); 
-    if (isset($_POST['doimk'])) {
+    if (!empty($_POST)) {
         $username = $_POST['username'];
         $password_cu = md5($_POST['password_cu']);
         $password_moi = md5($_POST['password_moi']);
@@ -15,10 +15,16 @@ require_once('connect_db.php');
         $count = mysqli_num_rows($row);
         if ($count > 0) {
             $sql_update = mysqli_query($conn,"UPDATE account SET password ='".$password_moi."'");
-            echo '<p style="color:green">Mật khẩu đã được thay đổi</p>';
-            header("Location:")
+            echo "<script>
+                    alert('Đổi mật khẩu thành công!');
+                    window.location.href = 'login.php';
+                </script>";
         } else {
-            echo '<p style="color="red">Tài khoản hoặc mật khẩu không đúng , vui lòng nhập lại.</p>';
+            echo "<script>
+                    alert('Sai tài khoản hoặc mật khẩu cũ! Vui lòng nhập lại');
+                    history.back();
+                </script>";
+                unset($_POST);
         }
     }
 ?>
@@ -26,10 +32,10 @@ require_once('connect_db.php');
 <body>
     <form method="post" action="" class="doimk">
         <h2 class="ten">Đổi Mật Khẩu</h2>
-        <div class="user">Username: <input type="text" name="username" value="" required> </div>
-        <div class="pass">Password: <input type="password" name="password_cu" value="" required/></div>
-        <div class="pass">A new password: <input type="password" name="password_moi" value="" required/></div>
-        </div><div class="btluuthaydoi"><input type="submit" type="button" id="tollbar" name="doimk" class="password"style="margin-top: 19px;" value="Lưu Thay Đổi" ></div>
+        <div class="user">Tên tài khoản: <input type="text" name="username" value="" required> </div>
+        <div class="pass">Mật khẩu: <input type="password" name="password_cu" value="" required/></div>
+        <div class="pass">Mật khẩu mới: <input type="password" name="password_moi" value="" required/></div>
+        </div><div class="btluuthaydoi"><input type="submit" type="button" id="tollbar" name="doimk" class="password" style="margin-top: 19px;" value="Lưu Thay Đổi" ></div>
     </form>
 </body>
 </html>
