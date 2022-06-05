f8b3ea3a4ec3a10ba548d6e7dacce9aa412f9b11
Vin <phamvanvinh8a2@gmail.com>
2022-06-06 01:28:43 +0700

    Update database, category

--- js/category.js
+++ js/category.js
@@ -11,7 +11,32 @@ let current_page = document.querySelector('.category-top-right>input[name="page"
 let form = document.querySelector('.category-top-right');
 pages.forEach(function(item) {
     item.onclick = function() {
+        console.log('cc');
         current_page.value = item.getAttribute('data-value');
+        removeUselessStuff();
         form.submit();
     }
 })
+
+let order = document.querySelector('select#order');
+let field = document.querySelector('input[name="field"]');
+let by = document.querySelector('input[name="by"]');
+order.onchange = function() {
+    let orderby = order.value.split(' ');
+    field.value = orderby[0] ? orderby[0] : '';
+    by.value = orderby[1] ? orderby[1] : '';
+}
+
+let size = document.querySelector('select#size');
+const stuff = [field, by, size];
+function removeUselessStuff() {
+    stuff.forEach(function(item) {
+        if (!item.value) {
+            item.name = '';
+        }
+    })
+    if (current_page.value == 1) {
+        current_page.name = '';
+    }
+    return true;
+}
