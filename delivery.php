b611257ee758ab59bff0ffba2c64c9577c89a4cf
Vin <phamvanvinh8a2@gmail.com>
2022-06-15 18:16:55 +0700

    Update base.css, delivery.css

--- delivery.php
+++ delivery.php
@@ -2,14 +2,11 @@
     require_once('site.php');
     loadHeader();
     require_once('modules/function/directToLoginIfNot.php');
-   
-    echo '	<link rel="stylesheet" href="style/styledelivery.css">
+    echo '<link rel="stylesheet" href="style/styledelivery.css">
     <script defer src="js/delivery.js"></script>">';
     loadMenu();
-    require_once('connect_db.php');
+
     require_once('modules/function/price.php');
-?>
-<?php 
     require('connect_db.php');
     $username = $_SESSION['username'];
     $sql = "SELECT `fullname`,`phone_number`,`shipping_address` FROM `account` where username = '$username' ";
@@ -52,117 +49,113 @@
                     alert("Đã thanh toán thành công!");
                     location.href = "index.php";
                     </script>';
-            }else{
+            } else {
                 echo '<script>
                     alert("Thanh toán thất bại, không có sản phẩm trong giỏ hàng!");
                     location.href = "index.php";
                     </script>';
             }
-           
         }
     }
 ?>
 <section>
-    <form class="delivery" method="POST">  
-    <div class="container">
-        <div class="delivery-content">
-            <h1>Địa Chỉ Giao Hàng</h1>
-            <div class="delivery-conten-above">
-                <table>
-                    <div class="delivery-conten-above-input-1 row">
-                        <div class="delivery-conten-above-input-item">
-                            <label class = "itemsthongtin1">Họ tên </label>
-                            <input name="fullname" value="<?php echo "$fullname" ?>" type="text" >
-                        </div>
-                        <div class="delivery-conten-above-input-item">
-                            <label class = "itemsthongtin2">Số điện thoại </label>
-                            <input class="thongtinsdt" name="SDT" value="<?php echo "$SDT" ?>" type="text" >
-                        </div>
-                        
-                    </div>
-                    <div class="delivery-conten-above-input-2 row">
-                        <div class="delivery-conten-above-input-2-item">
-                            <label class="diachigiaohang" for="">Địa chỉ giao hàng </label>
-                            <input name="Address" value="<?php echo "$Address" ?>" type="text" >
-                    </div>
-                </table>
-            </div>
-            <div class="delivery-conten-below">
-                <table>
-                    <div>
-                        <tr>
-                            <th>Tên sản phẩm</th>
-                            <th>Số lượng</th>
-                            <th>Size</th>
-                            <th>Thành tiền</th>
-                        </tr>
-                        <?php 
-                            require_once "connect_db.php";
-                            if(isset($_GET['id']))
-                            {
-                                $sql = "SELECT product.prod_name, cart.quantity, cart.size, product.price,  promotion.calc_unit, promo_price FROM cart JOIN product ON cart.prod_id = product.prod_id LEFT JOIN promotion ON product.promo_code = promotion.promo_code where username = '".$_SESSION['username']."' AND cart.prod_id='".$_GET['id']."' AND cart.size='".$_GET['size']."'";
-                                $result = $conn->query($sql);
-                                if($result->num_rows > 0){
-                                    $tongtien=0;
-                                    while ($rows = $result->fetch_array()) {
-                                        $sl_giohang = $rows[1];
-                                        $price_sp = $rows[3];
-                                        $donvi = $rows[4];
-                                        $gia = $rows[5];
-                                        $GiaSauGiam = getPrice($price_sp, $gia, $donvi)*$sl_giohang;
-                                        $numberformat = number_format($GiaSauGiam, 0, '', '.');
-                                        $tongtien=$tongtien+$GiaSauGiam;
-                                        echo "<tr>
-                                            <td>$rows[0]</td>
-                                            <td>$rows[1]</td>
-                                            <td class='size'>$rows[2]</td>
-                                            <td class='thanhtien'><p>$numberformat<sup>đ</sup></p></td>
-                                        </tr>";
-                                    }
-                                }
-                            }
-                            else{
-                            $sql = "SELECT product.prod_name, cart.quantity, cart.size, product.price,  promotion.calc_unit, promo_price FROM cart JOIN product ON cart.prod_id = product.prod_id LEFT JOIN promotion ON product.promo_code = promotion.promo_code where username = '".$_SESSION['username']."' ";
-                            $result = $conn->query($sql);
-                            $tongtien=0;
-                            if($result->num_rows > 0){
-                                while ($rows = $result->fetch_array()) {
-                                    $sl_giohang = $rows[1];
-                                    $price_sp = $rows[3];
-                                    $donvi = $rows[4];
-                                    $gia = $rows[5];
-                                    $GiaSauGiam = getPrice($price_sp, $gia, $donvi)*$sl_giohang;
-                                    $numberformat = number_format($GiaSauGiam, 0, '', '.');
-                                    $tongtien=$tongtien+$GiaSauGiam;
-                                    echo "<tr>
-                                        <td>$rows[0]</td>
-                                        <td>$rows[1]</td>
-                                        <td class='size'>$rows[2]</td>
-                                        <td class='thanhtien'><p>$numberformat<sup>đ</sup></p></td>
-                                    </tr>";
-                                }
-                            }
-                        }
-                        $tongtien=number_format($tongtien, 0, '', '.');
-                        ?>
-                        <?php
-                        echo '<tr>
-                            <td style="font-weight: bold;" colspan="3">Tổng tiền hàng</td>
-                            <td class="thanhtoan" style="font-weight: bold;" ><span>'.$tongtien.'</span><sup>đ</sup></td>
-                        </tr>'
-                        ?>
-                    </div>
-                </table>
-                <div class="delivery-content-left-button">
-                    <p><button style="font-weight: bold" type="button" onclick="quaylaigiohang()">&#8678; Quay lại giỏ hàng</button></p>
+    <form class="delivery-content" method="POST">
+        <h1>Địa Chỉ Giao Hàng</h1>
+        <div class="delivery-conten-above">
+            <div class="delivery-conten-above-input-1">
+                <div class="delivery-conten-above-input-item">
+                    <label class = "itemsthongtin1" for="fullname">Họ tên </label>
+                    <input name="fullname" id="fullname" value="<?php echo "$fullname" ?>" type="text" >
                 </div>
-                <div method="POST" class="delivery-content-right-button">
-                    <p><button name="tienvang" style="font-weight: bold" type="submit">THANH TOÁN</button></p>
+                <div class="delivery-conten-above-input-item">
+                    <label class = "itemsthongtin2" for="SDT">Số điện thoại </label>
+                    <input class="thongtinsdt" name="SDT" id="SDT" value="<?php echo "$SDT" ?>" type="text" >
+                </div>
+            </div>
+            <div class="delivery-conten-above-input-2">
+                <div class="delivery-conten-above-input-item">
+                    <label class="diachigiaohang" for="Address">Địa chỉ giao hàng </label>
+                    <textarea name="Address" id="Address"><?php echo "$Address"?></textarea>
                 </div>
             </div>
         </div>
-    </div>
-    </div>
+        <div class="delivery-conten-below">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Tên sản phẩm</th>
+                        <th>Số lượng</th>
+                        <th>Size</th>
+                        <th>Thành tiền</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                    require_once "connect_db.php";
+                    if (isset($_GET['id'])) {
+                        $sql = "SELECT product.prod_name, cart.quantity, cart.size, product.price,  promotion.calc_unit, promo_price FROM cart JOIN product ON cart.prod_id = product.prod_id LEFT JOIN promotion ON product.promo_code = promotion.promo_code where username = '".$_SESSION['username']."' AND cart.prod_id='".$_GET['id']."' AND cart.size='".$_GET['size']."'";
+                        $result = $conn->query($sql);
+                        if ($result->num_rows > 0) {
+                            $tongtien=0;
+                            while ($rows = $result->fetch_array()) {
+                                $sl_giohang = $rows[1];
+                                $price_sp = $rows[3];
+                                $donvi = $rows[4];
+                                $gia = $rows[5];
+                                $GiaSauGiam = getPrice($price_sp, $gia, $donvi)*$sl_giohang;
+                                $numberformat = number_format($GiaSauGiam, 0, '', '.');
+                                $tongtien=$tongtien+$GiaSauGiam;
+                                echo "<tr>
+                                    <td>$rows[0]</td>
+                                    <td>$rows[1]</td>
+                                    <td class='size'>$rows[2]</td>
+                                    <td class='thanhtien'><p>$numberformat<sup>đ</sup></p></td>
+                                </tr>";
+                            }
+                        }
+                    } else {
+                        $sql = "SELECT product.prod_name, cart.quantity, cart.size, product.price,  promotion.calc_unit, promo_price FROM cart JOIN product ON cart.prod_id = product.prod_id LEFT JOIN promotion ON product.promo_code = promotion.promo_code where username = '".$_SESSION['username']."' ";
+                        $result = $conn->query($sql);
+                        $tongtien=0;
+                        if ($result->num_rows > 0) {
+                            while ($rows = $result->fetch_array()) {
+                                $sl_giohang = $rows[1];
+                                $price_sp = $rows[3];
+                                $donvi = $rows[4];
+                                $gia = $rows[5];
+                                $GiaSauGiam = getPrice($price_sp, $gia, $donvi)*$sl_giohang;
+                                $numberformat = formatPrice($GiaSauGiam);
+                                $tongtien=$tongtien+$GiaSauGiam;
+                                echo "<tr>
+                                    <td>$rows[0]</td>
+                                    <td>$rows[1]</td>
+                                    <td class='size'>$rows[2]</td>
+                                    <td class='thanhtien'><p>$numberformat<sup>đ</sup></p></td>
+                                </tr>";
+                            }
+                        }
+                    }
+                    $tongtien=formatPrice($tongtien);
+
+                    echo '
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="3">Tổng tiền hàng</td>
+                            <td class="thanhtoan"><span>'.$tongtien.'</span><sup>đ</sup></td>
+                        </tr>
+                    </tfoot>';
+                ?>
+            </table>
+            <div class="delivery-content-below-button">
+                <div class="delivery-content-left-button">
+                   <button type="button" class="btn--white" onclick="quaylaigiohang()"><i class="fa-solid fa-left-long"></i> Quay lại giỏ hàng</button>
+                </div>
+                <div method="POST" class="delivery-content-right-button">
+                    <button name="tienvang" class="btn--black" type="submit">THANH TOÁN</button>
+                </div>
+            </div>
+        </div>
     </form>
 </section>
 <?php
